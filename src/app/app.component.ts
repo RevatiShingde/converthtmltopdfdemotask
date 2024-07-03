267a938a34baae261fc33394ba53d2bd1e8d123c
ravati shingde <vaibhavshingde@gmail.com>
2024-07-03 12:29:21 +0530

    added css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,7 @@ import html2canvas from 'html2canvas';
 })
 export class AppComponent {
   title = 'pdf-generator';
+  companyLogo: string = "psm Tech";
   employeeData = [
     {
       id : 1,
@@ -78,50 +79,7 @@ export class AppComponent {
     },
   ] 
   
-//  generatePDF() {
-//   const doc = new jsPDF({
-//     orientation: 'p',  
-//     unit: 'mm',        
-//     format: 'a4'       
-//   });
-
-  
-//   const contentElement : any | null = document.getElementById('pdf-content');
-
-//   const options = {
-//     background: '#fff',   
-//     scale: 1,            
-//     logging: true         
-//   };
-
-//   html2canvas(contentElement, options).then(canvas => {
-//     const imgData = canvas.toDataURL('image/png');
-//     const imgProps = doc.getImageProperties(imgData);
-//     const pdfWidth = doc.internal.pageSize.getWidth();
-//     let pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
-
-//     doc.addImage(imgData, 'PNG', 0, 0, pdfWidth, pdfHeight);
-
-//     let remainingHeight = imgProps.height - pdfHeight;
-//     let currentPage = 1;
-
-//     while (remainingHeight > 0) {
-//       doc.addPage();  
-
-//       pdfHeight = Math.min(imgProps.height, remainingHeight);
-//       doc.addImage(imgData, 'PNG', 0, -imgProps.height + remainingHeight, pdfWidth, pdfHeight);
-
-//       remainingHeight -= pdfHeight;
-//       currentPage++;
-
-//       doc.text(`Page ${currentPage}`, pdfWidth / 2, pdfHeight + 10, { align: 'center' });
-//     }
-
-//     doc.save('sample.pdf');
-//   });
-// }
-
-generatePDF() {
+  generatePDF() {
   const DATA  : any | null= document.getElementById('pdf-content');
   const pdf = new jsPDF('p', 'mm', 'a4');
   const options: any = {
